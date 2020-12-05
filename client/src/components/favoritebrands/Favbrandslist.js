426e034a9abcb3f95365ed2f03ca290a4c408da4
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-12-05 17:48:54 +0100

    basic front sur favbrandslist.js done

--- client/src/components/favoritebrands/Favbrandslist.js
+++ client/src/components/favoritebrands/Favbrandslist.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import service from '../auth/auth-service';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 class Favbrandslist extends Component {
     state = { 
@@ -10,9 +12,9 @@ class Favbrandslist extends Component {
     getAllFavBrands = () =>{
         service.get(`/myfavoritebrands`)
           .then(responseFromApi => {
-              console.log(responseFromApi.data)
+              console.log(responseFromApi.data.currentUser.favoritebrands)
             this.setState({
-                listOfFavbrands: responseFromApi.data
+                listOfFavbrands: responseFromApi.data.currentUser.favoritebrands
             })
           })
           .catch(err => console.log('Error while fetching brands', err))
@@ -23,18 +25,26 @@ class Favbrandslist extends Component {
       }
 
     render() {
-
         return (
-            <div>
-                Hello Elo
-                {/* { this.props.user.favoritebrands.map( brand => {
-                return (                
-                  <div>
-                      {brand}
-                  </div>
-                )}
-              )} */}
-            </div>
+            <Container>
+
+                <h3>MY BRANDS</h3>
+
+                <button>ADD BRAND</button>
+
+                <Row>                    
+                    { this.state.listOfFavbrands.map( brand => {
+                        return (                
+                        <div key={brand._id}>
+                        <Col>
+                            <img    className="wallet-logoimg-style"
+                                    src={brand.logoUrl}/>
+                        </Col>
+                        </div>
+                        )}
+                    )}
+                </Row>
+            </Container>
         );
     }
 }
