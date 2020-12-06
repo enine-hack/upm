38e9b55a6d9eb77a6d91157bdb3eef01f93341ed
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-12-06 22:41:39 +0100

    responsive xs screen pour brandslist on going

--- client/src/components/brands/Brandslist.js
+++ client/src/components/brands/Brandslist.js
@@ -27,83 +27,106 @@ class Brandslist extends Component {
       return(
         
           <div className="brandslist_public">
-            <div>
-                <h1 className="brandslist_public__title">
-                    Discover
-                </h1>
-            </div>
-
-            <div>
-              <p>OUR MOST ADDED BRANDS</p>
-            </div>
-
-            <div>
-            { this.state.listOfBrands.splice(Math.floor(Math.random()*this.state.listOfBrands.length),20).map( brand => {
-              return (                
-                <div
-                      key={brand._id}>
-                      { brand.popularity > 2 ? (
-                      
-                        <>  
-                          <Link to={`/brandslist/${brand._id}`}>
-                              <img className="brandslist_public__logo"
-                                   src={brand.logoUrl} />
-                          </Link>
-
-                          <Link to={`/brandslist/${brand._id}`}>
-                            {brand.brandname}
-                          </Link>
-
-                          <Link to={`/brandslist/${brand._id}`}>
-                            {/* <img src={"https://cdn0.iconfinder.com/data/icons/micro8/100/micro8icons-go-36-512.png"}/> */}
-                          </Link>
-                        </>
-
-                        ) : (
-
-                        <></>
-                    )}
-
-                </div>
-                
-              )}
-              
-            )}
-            </div>
-
-
-            {/* A */}
-            <div><p>A</p></div>
+            <img  className="brandslist_public__cover_img"
+                  src="https://images.unsplash.com/photo-1548508839-a31a4673f82d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"/>
             
-            <div>
-            { this.state.listOfBrands.map( brand => {
-              return (                
-                <div 
-                      key={brand._id}>
-                      { brand.brandname.startsWith('A') ? (
-                        <div>
-                        
-                          <Link to={`/brandslist/${brand._id}`}>
-
-                            <img  className="brandslist_public__logo"
-                                  src={brand.logoUrl} />
-                          
-                              {brand.brandname}
-                          
-                          
-                            {/* <img src={"https://cdn0.iconfinder.com/data/icons/micro8/100/micro8icons-go-36-512.png"}/> */}
-
-                          </Link>
-                        </div>
-                        
-                          ) : (
-                          <></>
-                    )}
-
-                </div>
-              )}
-            )}
+            <div className="brandslist_public__cover_title">
+              <p>Go for prestigious <br></br> and creation</p>
             </div>
+            
+            <div className="brandslist_public__l">
+                <div>
+                    <p className="brandslist_public__title">
+                        Discover
+                    </p>
+                </div>
+
+                <div>
+                    <p className="brandslist_public__description">
+                        ALL THE BRANDS
+                    </p>
+                </div>
+
+
+
+                <div className="brandslist_public__most_added">
+                    <p className="brandslist_public__most_added_row_title">
+                      MOST ADDED
+                    </p>
+                
+                { this.state.listOfBrands.splice(Math.floor(Math.random()*this.state.listOfBrands.length),20).map( brand => {
+                  return (
+                                    
+                    <div className="brandslist_public__most_added_row"
+                          key={brand._id}>
+                          { brand.popularity > 2 ? (
+                          
+                            <>  
+                            <div className="brandslist_public_most_added_col1">
+                                <Link to={`/brandslist/${brand._id}`}>
+                                    <img className="brandslist_public__logo"
+                                        src={brand.logoUrl} />
+                                </Link>
+
+                                <Link to={`/brandslist/${brand._id}`}>
+                                    <p  className="brandslist_public__brandname">
+                                        {brand.brandname}
+                                    </p>
+                                </Link>
+                             </div>
+
+                              <Link to={`/brandslist/${brand._id}`}>
+                                <img  className="brandslist_public__showmore_icon"
+                                      src={"https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-more-512.png"}/>
+                              </Link>
+                            </>
+
+                            ) : (
+
+                            <></>
+                        )}
+
+                    </div>
+                    
+                  )}
+                  
+                )}
+                </div>
+
+
+                {/* A */}
+                <div><p>A</p></div>
+                
+                <div>
+                { this.state.listOfBrands.map( brand => {
+                  return (                
+                    <div 
+                          key={brand._id}>
+                          { brand.brandname.startsWith('A') ? (
+                            <div>
+                            
+                              <Link to={`/brandslist/${brand._id}`}>
+
+                                <img  className="brandslist_public__logo"
+                                      src={brand.logoUrl} />
+                              
+                                  {brand.brandname}
+                              
+                              
+                                {/* <img src={"https://cdn0.iconfinder.com/data/icons/micro8/100/micro8icons-go-36-512.png"}/> */}
+
+                              </Link>
+                            </div>
+                            
+                              ) : (
+                              <></>
+                        )}
+
+                    </div>
+                  )}
+                )}
+                </div>
+              </div>
         </div>
     
       )
