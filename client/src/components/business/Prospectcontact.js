4426f0cd8792e53b3fb77a6ecbd89b4ccb545f7c
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-12-15 14:36:29 +0100

    FRONT CSS for Prospectcontact.js OK

--- client/src/components/business/Prospectcontact.js
+++ client/src/components/business/Prospectcontact.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import service from '../auth/auth-service';
 
 
-
 class Prospectcontact extends Component {
     state = { 
         society: '',
@@ -12,6 +11,7 @@ class Prospectcontact extends Component {
         lastname: '',
         phone: '',
         email: '',
+        comments: '',
         error: '',
         confirmation:''
     }
@@ -31,6 +31,7 @@ handleChange = (event) => {
     const lastname = this.state.lastname;
     const phone = this.state.phone;
     const email = this.state.email;
+    const comments = this.state.comments
     service.post('/partners', { 
         society, 
         activity, 
@@ -38,7 +39,8 @@ handleChange = (event) => {
         firstname, 
         lastname,
         phone,
-        email})
+        email,
+        comments})
       .then(response => {
         this.setState({ 
             society: '',
@@ -48,8 +50,11 @@ handleChange = (event) => {
             lastname: '',
             phone: '',
             email: '',
+            comments: '',
             error: '',
-            confirmation: "Your request has been sent. Thank you for your interest, you will be contacted soon."});
+            confirmation: "Your request has been sent. Thank you for your interest, you will be contacted soon."
+        });
+        
         
       })
       .catch(err => this.setState({error: err.response.data.message}))
@@ -58,58 +63,69 @@ handleChange = (event) => {
     
     render() {
         return (
-            <div className="">
-                <div>
-                    <p  className=""> 
-                        Let's get started.</p>
-                </div>       
+            <div className="partners">
+
+                <div className="partners__bloc">
+                    <p className="partners__bloc_title">
+                            Become a partner
+                    </p>
+                    <p className="partners__bloc_desc">
+                        LET'S GET STARTED
+                    </p>     
 
                 <div >
-                    <form   className=""
-                            onSubmit={this.handleFormSubmit}>
+                    <form onSubmit={this.handleFormSubmit}>
 
                         
-                        <div className="">
-                            <input className=""
+                        <div className="partners__form">
+                            <input className="partners__form_input"
                                     type="text" name="society"  value={this.state.society}  placeholder="Society"
                                     onChange={(event) =>this.handleChange(event)}/>
                             
-                            <input className=""
+                            <input className="partners__form_input"
                                     type="text" name="activity"  value={this.state.activity} placeholder="Activity"
                                     onChange={(event) =>this.handleChange(event)}/>
                         
-                            <input className=""
+                            <input className="partners__form_input"
                                     type="text" name="country"  value={this.state.country} placeholder="Country"
                                     onChange={(event) =>this.handleChange(event)}/>
                             
-                            <input className=""
+                            <input className="partners__form_input"
                                     type="text" name="firstname"  value={this.state.firstname}  placeholder="Firstname"
                                     onChange={(event) =>this.handleChange(event)}/>
                             
-                            <input className=""
+                            <input className="partners__form_input"
                                     type="text" name="lastname"  value={this.state.lastname} placeholder="Lastname"
                                     onChange={(event) =>this.handleChange(event)}/>
                         
-                            <input className=""
+                            <input className="partners__form_input"
                                     type="text" name="phone"  value={this.state.phone} placeholder="Mobile"
                                     onChange={(event) =>this.handleChange(event)}/>
 
-                            <input className=""
+                            <input className="partners__form_input"
                             type="text" name="email"  value={this.state.email} placeholder="Email"
                             onChange={(event) =>this.handleChange(event)}/>
 
-                            <button className="">
+                            <input className="partners__form_input_textarea"
+                            type="textarea" name="comments"  value={this.state.comments} placeholder="Message"
+                            onChange={(event) =>this.handleChange(event)}/>
+
+                            <button className="partners__form_btn">
                                         SUBMIT</button>
                                         
                                             
                             {/* Affichage message d'erreur */}
                                 {this.state.error && (
-                                    <p className="error_form_msg">{this.state.error}
+                                    <p className="partners_error_form_msg">{this.state.error}
+                                    
                                     </p>
+                                   
+                                        
+                                    
                                 )}
 
                              {/* Affichage du message de confirmation logged */}
-                            <p className="confirm_form_msg">{this.state.confirmation}</p>
+                            <p className="partners_confirm_form_msg">{this.state.confirmation}</p>
                         </div>
                         
                        
@@ -117,7 +133,7 @@ handleChange = (event) => {
 
                 </div>
                 
-
+            </div>
                       
            
             </div>
